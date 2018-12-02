67e79bea46ceece1b557688e02e963ea010bb32d
Daniel Chamorro <dan@dcitllc.com>
2018-12-02 01:33:24 -0500

    Redering Video titles and thumbnails

--- src/component/VideoItem.js
+++ src/component/VideoItem.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const VideoItem = () => {
+const VideoItem = ({ video }) => {
   return (
     <div>
-      <h1>Video Item</h1>
+      <img src={video.snippet.thumbnails.medium.url} alt="" />
+      {video.snippet.title}
     </div>
   );
 };
