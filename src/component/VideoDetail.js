ed0b4b6c46e6369e373cc97f1fb51c962266bf37
Daniel Chamorro <dan@dcitllc.com>
2018-12-02 02:47:59 -0500

    Added Styling and Cleaned up some Errors - Fixed key error for mapping - Added alt tag to img - Addd grid type styles using semantic-ui

--- src/component/VideoDetail.js
+++ src/component/VideoDetail.js
@@ -10,7 +10,7 @@ const VideoDetail = ({ video }) => {
   return (
     <div>
       <div>
-        <iframe src={videoSrc} />
+        <iframe src={videoSrc} title="video player" />
       </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
