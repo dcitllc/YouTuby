fdc42e16d9f4a8d42e64e22dda3e4778ccfe6a45
Daniel Chamorro <dan@dcitllc.com>
2018-12-02 02:02:28 -0500

    Defined new callback method onVideoSelect(), then passed it as a prop down to VideoList, then pass it down again to VideoItem, now when user clicks on a single video we invoke the callback

--- src/component/App.js
+++ src/component/App.js
@@ -5,7 +5,8 @@ import VideoList from "./VideoList";
 
 class App extends Component {
   state = {
-    videos: []
+    videos: [],
+    selectedVideo: null
   };
 
   onTermSubmit = async term => {
@@ -18,11 +19,18 @@ class App extends Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = video => {
+    console.log("From the App!", video);
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
