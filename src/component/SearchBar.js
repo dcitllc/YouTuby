c167858d4665307e2f429118675167a15201350c
Daniel Chamorro <dan@dcitllc.com>
2018-12-01 22:30:40 -0500

    Added event handler for form

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -3,12 +3,20 @@ import React, { Component } from "react";
 class SearchBar extends Component {
   state = { term: "" };
 
-  onInputChange = () => {};
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    //TODO: Make sure we call callback from parent
+  };
 
   render() {
     return (
       <div className="ui segment search-bar ">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label htmlFor="">Video Search</label>
             <input
