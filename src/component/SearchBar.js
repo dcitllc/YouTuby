d91f39245c53e4c78b4beb9025d9567c7fcdb0b0
Daniel Chamorro <dan@dcitllc.com>
2018-12-01 22:24:48 -0500

    Added some styles with semantic for SearchBar

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  state = { term: "" };
+
+  onInputChange = () => {};
+
   render() {
     return (
-      <div>
-        <h1>Search Bar</h1>
+      <div className="ui segment search-bar ">
+        <form className="ui form">
+          <div className="field">
+            <label htmlFor="">Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
       </div>
     );
   }
